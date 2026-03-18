27b900e01b1ed79b0ccc90301b27dba37d3f9220
arelhan <are255gu@gmail.com>
2026-03-18 16:59:07 +0300

    Docker desteği eklendi

--- public-app/next.config.js
+++ public-app/next.config.js
@@ -9,5 +9,13 @@ const nextConfig = {
       },
     ]
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.BACKEND_URL || 'http://localhost:4000'}/api/:path*`,
+      },
+    ]
+  },
 }
 module.exports = nextConfig
